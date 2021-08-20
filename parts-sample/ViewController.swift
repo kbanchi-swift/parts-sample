ae7bf2c20cf8752194af254841aa6285d17a8471
kbanchi <b.keisuke1111@gmail.com>
2021-08-20 13:55:03 +0900

    Initial Commit

---
+++ parts-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parts-sample
+//
+//  Created by 伴地慶介 on 2021/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
